89593d4eee80939fb3f22461a3569ea13168ddc7
hxzhu <hxzhu@umich.edu>
2020-03-29 19:03:01 -0400

    add requirements

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='basenet',
-    author='Ben Johnson',
-    author_email='bkj.322@gmail.com',
+    author='Haoxuan Zhu',
+    author_email='hxzhu@umich.edu',
     classifiers=[],
-    description='pytorch training tools',
-    keywords=['basenet'],
+    description='Host version: NBSGD for transmuter',
+    keywords=['nbsgd'],
     license='ALV2',
     packages=find_packages(),
     version="0.0.0"
